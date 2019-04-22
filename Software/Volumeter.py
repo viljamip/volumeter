b167b48898be681f331c68386d984946d2be56ff
Viljami Pirttimaa <viljami.pirttimaa@gmail.com>
2019-04-22 14:33:50 +0300

    Tested nb extensions

--- Software/Volumeter.py
+++ Software/Volumeter.py
@@ -3,28 +3,21 @@ import pandas as pd
 import numpy as np
 import time
 
-motionControlPort = '/dev/ttyUSB1'
-sensorPort = '/dev/ttyUSB0'
 loadCellConstants = None
 mcSerial = None
 sensorSerial = None
 
-class Volumeter:
-    def initSensors(self):
-        print("initSensors()")
-        self.loadCellConstants = self.readLoadCellCalibration()
+def initSensors(sensorPort):
+    print("initSensors()")
+    loadCellConstants = readLoadCellCalibration()
 
-        print(sensorPort)
-        #sensorSerial = serial.Serial(port=sensorPort, baudrate=115200)
+    print(sensorPort)
+    #sensorSerial = serial.Serial(port=sensorPort, baudrate=115200)
 
-    def temp(self):
-        print(self.loadCellConstants)
-    
-    def readLoadCellCalibration(self):
-        cal = np.genfromtxt('loadCellCalibration.csv', delimiter=',')
-        return cal
+def temp():
+    print(loadCellConstants)
 
-    def __init__(self):
-        print(motionControlPort)
-        self.initSensors()
+def readLoadCellCalibration():
+    cal = np.genfromtxt('loadCellCalibration.csv', delimiter=',')
+    return cal
 
