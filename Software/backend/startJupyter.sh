289fc0b1340297edfc79ca034248cb0a2884ebf9
Viljami Pirttimaa - Volumeter <juuso.pirttimaa@aalto.fi>
2019-11-12 10:44:49 +0200

    Startup script added

---
+++ Software/backend/startJupyter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add the following line to /etc/rc.local right before exit 0
+# su pi -c 'bash /home/pi/Desktop/dimensiometer/Software/backend/startJupyter.sh'
+
+source /home/pi/.profile
+workon volumeter2
+jupyter notebook --notebook-dir=/home/pi/Desktop/dimensiometer/Software
+
