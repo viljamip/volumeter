def27b181ef19d6c68b10c5490c4711aee5dd798
Viljami Pirttimaa - Volumeter <juuso.pirttimaa@aalto.fi>
2019-11-19 09:49:07 +0200

    GUI test

--- Software/backend/startJupyter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Add the following line to /etc/rc.local right before exit 0
-# su pi -c 'sh /home/pi/Desktop/dimensiometer/Software/backend/startJupyter.sh'
-
-source /home/pi/.profile
-workon volumeter2
-jupyter notebook --notebook-dir=/home/pi/Desktop/dimensiometer/Software
-
